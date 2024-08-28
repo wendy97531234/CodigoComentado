e3643fff56e28f99c922a12fe2606b51e1bdfb6a
wendy97531234 <126539753+wendy97531234@users.noreply.github.com>
2024-08-27 21:35:30 -0600

    :)comentadoDSD

--- sqlServer_visualEstudio_Conectado_DataProvider-main/DataSourceDemo/Program.cs
+++ sqlServer_visualEstudio_Conectado_DataProvider-main/DataSourceDemo/Program.cs
@@ -10,12 +10,18 @@ namespace DataSourceDemo
     {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
+        /// Este método es el primer lugar donde el código de la aplicación comienza a ejecutarse.
         /// </summary>
-        [STAThread]
+        [STAThread] // Indica que la aplicación usa el modelo de subprocesamiento de un solo apartamento.
         static void Main()
         {
+            // Habilita estilos visuales para la aplicación. Esto permite que la aplicación tenga un aspecto moderno y consistente con el sistema operativo.
             Application.EnableVisualStyles();
+
+            // Establece que la aplicación utilice un método de renderizado de texto compatible, necesario para algunas configuraciones de visualización.
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Inicia la aplicación y muestra el formulario principal (Form2).
             Application.Run(new Form2());
         }
     }
